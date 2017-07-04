0f2d02cdf50c495377b4de710082d7032f8a6372
lucasgbd <lucasgbd@gmail.com>
2017-07-04 14:11:05 +0100

    Componentes adicionados no ClientesComponent e componente de exibicao de erros adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { NotasFiscaisComponent } from './notasfiscais/notasfiscais.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { FormDebugComponent } from './form-debug/form-debug.component';
+import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +21,14 @@ import { FormDebugComponent } from './form-debug/form-debug.component';
     NotasFiscaisComponent,
     HomeComponent,
     LoginComponent,
-    FormDebugComponent
+    FormDebugComponent,
+    CampoControlErroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-clientes',
@@ -7,7 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+  verificaValidTouched(campo) {
+    return !campo.valid && campo.touched;
+  }
+
+  aplicaCssErro(campo) {
+    return {'has-danger': this.verificaValidTouched(campo)};
+  }
+
+  consultaCEP(cep, form) {
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != "") {
+      var validacep = /^[0-9]{8}$/;
+
+      if(validacep.test(cep)) {
+
+        this.resetaDadosForm(form);
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+        .map(dados => dados.json())
+        .subscribe(dados => this.populaDadosForm(dados,form));
+
+      }
+    }
+  }
+
+  populaDadosForm(dados, formulario) {
+     formulario.form.patchValue({
+      endereco: {
+        cep: dados.cep,
+        numero:"" ,
+        rua: dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        uf: dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm(formulario) {
+    formulario.form.patchValue({
+      endereco: {
+        cep: null,
+        numero: null,
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        uf: null
+      }
+    });
+  }
+
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
