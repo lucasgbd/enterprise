24eb4164f74c4f5a3e54bc94af9f6523d672c3d6
lucasgbd <lucasgbd@gmail.com>
2017-07-04 09:29:46 +0100

    Insercao dos componentes no form de login e criacao do form-debug.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/Forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,7 @@ import { ClientesComponent } from './clientes/clientes.component';
 import { NotasFiscaisComponent } from './notasfiscais/notasfiscais.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { FormDebugComponent } from './form-debug/form-debug.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { LoginComponent } from './login/login.component';
     ClientesComponent,
     NotasFiscaisComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    FormDebugComponent
   ],
   imports: [
     BrowserModule,
